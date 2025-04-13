7307eb90549fff1f12f8c5b8dbe205dfa75628a5
Nikita Yablochnikov <yablochnikovnikita@gmail.com>
2025-04-13 16:22:57 +0300

    added: news page

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 import Header from "./Header.js";
 import TabsCollection from "./Tabs.js";
+import VideoPlayerCollection from "./VideoPlayer.js";
 
 new Header();
 new TabsCollection();
+new VideoPlayerCollection();
